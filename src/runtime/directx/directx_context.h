c5e1eee82915bea10a947217956df4c7ba762766
Wenxiang Hu <Wenxiang.Hu@Microsoft.com>
2022-03-14 17:36:39 +0800

    Change experimental id;

--- src/runtime/directx/directx_context.h
+++ src/runtime/directx/directx_context.h
@@ -15,13 +15,8 @@ class DirectXContext {
     // Enable experimental features on device:
     //  - Such as running dxil without signed, which need Windows be in Developer mode
     //  - URL: https://github.com/microsoft/DirectXShaderCompiler/wiki/Running-Shaders
-    const GUID D3D12ExperimentalShaderModelsID = {/* 76f5573e-f13a-40f5-b297-81ce9e18933f */
-                                                  0x76f5573e,
-                                                  0xf13a,
-                                                  0x40f5,
-                                                  {0xb2, 0x97, 0x81, 0xce, 0x9e, 0x18, 0x93, 0x3f}};
     ThrowIfFailed(
-        D3D12EnableExperimentalFeatures(1, &D3D12ExperimentalShaderModelsID, nullptr, nullptr));
+        D3D12EnableExperimentalFeatures(1, &D3D12ExperimentalShaderModels, nullptr, nullptr));
 
     _devices.clear();
     // Get all qualified devices;
